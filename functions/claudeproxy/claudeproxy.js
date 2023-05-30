2ec8bf6aad3e08d164778e6007c9ee28a61f79a6
Hunar Batra <35395835+hunarbatra@users.noreply.github.com>
2023-05-30 15:03:36 +0100

    Update claudeproxy.js

--- functions/claudeproxy/claudeproxy.js
+++ functions/claudeproxy/claudeproxy.js
@@ -1,33 +1,36 @@
-// functions/proxy.js
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
-    if (event.httpMethod !== "POST") {
-        return { statusCode: 405, body: "Method Not Allowed" };
-    }
-
-    const req_data = JSON.parse(event.body);
-    const headers = {
-        'Content-Type': 'application/json',
-        'x-api-key': event.headers['x-api-key'] || ''
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 400,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({error: 'This endpoint supports only POST requests'}),
     };
+  }
 
-    try {
-        const response = await axios.post('https://api.anthropic.com/v1/complete', req_data, { headers });
-        return {
-            statusCode: 200,
-            body: JSON.stringify(response.data),
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Content-Type, x-api-key'
-            }
-        };
-    } catch (error) {
-        return {
-            statusCode: error.response.status,
-            body: JSON.stringify(error.response.data)
-        };
-    }
+  const reqData = JSON.parse(event.body);
+  const headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': event.headers['x-api-key'],
+  };
+
+  try {
+    const response = await axios.post('https://api.anthropic.com/v1/complete', reqData, { headers });
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type,x-api-key',
+        'Access-Control-Allow-Methods': 'GET,POST',
+      },
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({error: 'Internal Server Error'}),
+    };
+  }
 };
-
