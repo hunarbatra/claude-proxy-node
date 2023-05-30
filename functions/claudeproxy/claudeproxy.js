98cf9cbaa94578312709674bf9cdc31d0be5df1b
Hunar Batra <35395835+hunarbatra@users.noreply.github.com>
2023-05-30 15:11:29 +0100

    added cors preflight request

--- functions/claudeproxy/claudeproxy.js
+++ functions/claudeproxy/claudeproxy.js
@@ -1,35 +1,46 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type,x-api-key',
+    'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+  };
+
+  // Return CORS headers for preflight request
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers,
+      body: '',  // body must be empty
+    };
+  }
+
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 400,
-      headers: { 'Content-Type': 'application/json' },
+      headers,
       body: JSON.stringify({error: 'This endpoint supports only POST requests'}),
     };
   }
 
   const reqData = JSON.parse(event.body);
-  const headers = {
+  const requestHeaders = {
     'Content-Type': 'application/json',
     'x-api-key': event.headers['x-api-key'],
   };
 
   try {
-    const response = await axios.post('https://api.anthropic.com/v1/complete', reqData, { headers });
+    const response = await axios.post('https://api.anthropic.com/v1/complete', reqData, { headers: requestHeaders });
     return {
       statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type,x-api-key',
-        'Access-Control-Allow-Methods': 'GET,POST',
-      },
+      headers,
       body: JSON.stringify(response.data),
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers,
       body: JSON.stringify({error: 'Internal Server Error'}),
     };
   }
